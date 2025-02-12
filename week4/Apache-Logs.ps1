dd434a479c655142e5e4ae6b7eb0ce7ffa6d5ab6
seraphim gerbs <seraphim.gerber@mymail.champlain.edu>
2025-02-11 21:21:37 -0500

    Adding stuff

---
+++ week4/Apache-Logs.ps1
@@ -0,0 +1,26 @@
+﻿function Get-ApacheLogIPs {
+    param (
+        [string]$Page,
+        [string]$HttpCode,
+        [string]$Browser
+    )
+
+    $logFile = "C:\xampp\apache\logs\access.log"
+
+    $notfounds = Get-Content -Path $logFile | Where-Object {
+        ($_ -ilike "*$Page*") -and ($_ -match "\b$HttpCode\b") -and ($_ -ilike "*Browser*")
+    }
+
+    $ipsUnorganized = $notfounds | ForEach-Object {
+        ($_ -split '\s+')[0]
+    }
+
+    $ipCounts = $ipsUnorganized | Where-Object { $_ -ne "" } | Group-Object | Sort-Object Count -Descending
+
+    if ($ipCounts) {
+        Write-Host "IP addresses that visited '$Page' using '$Browser' and got HTTP response '$HttpCode':`n" 
+        $ipCounts | Format-Table Name, Count -AutoSize
+    } else {
+        Write-Host "No matching logs found for '$Page', HTTP Code: '$HttpCode', Browser: '$Browser'."
+    }
+}
---
+++ week4/apachelogs.ps1
@@ -0,0 +1,10 @@
+﻿$notfounds = Get-Content -Path C:\xampp\apache\logs\access.log | Select-String -Pattern '404'
+
+$regex = [regex] '\b(?:\d{1,3}\.){3}\d{1,3}\b'
+
+$ipsUnorganized = $notfounds | ForEach-Object { $regex.Match($_).Value }
+
+$ips = $ipsUnorganized | Where-Object { $_ -ne "" } | Sort-Object -Unique
+
+$ips
+
