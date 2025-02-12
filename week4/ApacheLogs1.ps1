7b669a9050f6e90cbefb65c0799734407fc4a32a
seraphim gerbs <seraphim.gerber@mymail.champlain.edu>
2025-02-12 16:00:01 -0500

    Second part

---
+++ week4/ApacheLogs1.ps1
@@ -0,0 +1,22 @@
+﻿function ApacheLogs1 {
+$logsNotFormatted = Get-Content -Path C:\xampp\apache\logs\access.log
+$tableRecords = @()
+
+for($i=0; $i -lt $logsNotFormatted.Count; $i++) {
+
+$words = $logsNotformatted[$i].Split(" ");
+
+ $tableRecords += [PSCustomObject]@{
+   "IP" = $words[0];
+   "Time" = $words[3];
+   "Method" = $words[5];
+   "Page" = $words[6];
+   "Protocol" = $words[7];
+   "Response" = $words[8];
+   "Referrer" = $words[9];
+   "Client" = $words[10]; }
+}
+return $tableRecords | Where-Object { $_.IP -match '10.*' }
+}
+$tableRecords = ApacheLogs1
+$tableRecords | Format-Table -AutoSize -Wrap
