edc67cc79042b132c53f57367c8626174d565e1d
seraphim gerbs <seraphim.gerber@mymail.champlain.edu>
2025-03-05 15:02:40 -0500

    update

--- week7/email.ps1
+++ week7/email.ps1
@@ -1,15 +1,12 @@
-﻿function SendAlertEmail {
+﻿function SendAlertEmail($Body) {
 
-$From = "seraphim.gerber@mymail.champlain.edu"
-$To = "seraphim.gerber@mymail.champlain.edu"
-$Subject = "Suspicious Activity!!!"
-$Body = "BODY OF EMAIL!"
+    $From = "seraphim.gerber@mymail.champlain.edu"
+    $To = "seraphim.gerber@mymail.champlain.edu"
+    $Subject = "Suspicious Activity!!!"
 
-$Password = "password" | ConvertTo-SecureString -AsPlainText -Force
-$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $From, $Password
+    $Password = "password" | ConvertTo-SecureString -AsPlainText -Force
+    $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $From, $Password
 
-Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -SmtpServer smtp.gmail.com -Port 587 -UseSsl -Credential $Credential
+    Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -SmtpServer smtp.gmail.com -Port 587 -UseSsl -Credential $Credential
 
 }
-
-SendAlertEmail 
