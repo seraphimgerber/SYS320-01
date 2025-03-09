34fbcc8f2a990fff6759e711497e43d4a9b51cf5
seraphim gerbs <seraphim.gerber@mymail.champlain.edu>
2025-03-09 15:40:53 -0400

    yes

---
+++ week8/get IOC.ps1
@@ -0,0 +1,32 @@
+﻿function Get-IOC {
+    param (
+        [string]$url = http://10.0.17.6/IOC.html"
+    )
+
+    $webContent = Invoke-WebRequest -Uri $url -UseBasicParsing
+
+    $html = New-Object -ComObject "HTMLFile"
+    $html.IHTMLDocument2_write($webContent.Content)
+
+    $rows = $html.getElementsByTagName("tr")
+
+    $IOC_List = @()
+
+    for ($i = 1; $i -lt $rows.length; $i++) {
+        $cols = $rows[$i].getElementsByTagName("td")
+        if ($cols.length -eq 2) {
+            $pattern = $cols[0].innerText.trim()
+            $description = $cols[1].innerText.trim()
+
+            $IOC_List += [PSCustomObject]@{
+                Pattern = $pattern
+                Explanation = $description
+            }
+        }
+    }
+
+    return $IOC_List
+
+}
+
+Get-IOC
