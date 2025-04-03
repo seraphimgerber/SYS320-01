4b28a3430e4ed962cd75285cf7ba97ec82d3713c
seraphim gerbs <seraphim.gerber@mymail.champlain.edu>
2025-04-03 13:43:04 -0400

    updating

---
+++ week12/apacheLogMenu-1.bash
@@ -0,0 +1,141 @@
+#! /bin/bash
+
+logFile="/var/log/apache2/access.log"
+
+function displayAllLogs(){
+	cat "$logFile"
+}
+
+function displayOnlyIPs(){
+        cat "$logFile" | cut -d ' ' -f 1 | sort -n | uniq -c
+}
+
+function displayOnlyPages(){
+	cat "$logFile" | cut -d ' ' -f 7 | tr -d '/' | sort -n | uniq -c
+}
+
+function histogram(){
+
+	local visitsPerDay=$(cat "$logFile" | cut -d " " -f 4,1 | tr -d '['  | sort \
+                              | uniq)
+	# This is for debugging, print here to see what it does to continue:
+	#  echo "$visitsPerDay"
+
+        :> newtemp.txt  # what :> does is in slides
+	echo "$visitsPerDay" | while read -r line;
+	do
+		local withoutHours=$(echo "$line" | cut -d " " -f 2 \
+                                     | cut -d ":" -f 1)
+		local IP=$(echo "$line" | cut -d  " " -f 1)
+          
+		local newLine="$IP $withoutHours"
+		echo "$IP $withoutHours" >> newtemp.txt
+	done 
+	cat "newtemp.txt" | sort -n | uniq -c
+}
+
+function frequentVisitors(){
+	
+	while read -r line;
+	do
+		ip=$(echo "$line" | awk '{print $1}')
+
+		count[$ip]=$((count[$ip] + 1))
+	done < "$logFile"
+
+	for ip in "${!count[@]}"; do
+		if (( count[$ip] > 10 )); then
+			echo "$ip"
+		fi
+	done
+}
+# function: frequentVisitors: 
+# Only display the IPs that have more than 10 visits
+# You can either call histogram and process the results,
+# Or make a whole new function. Do not forget to separate the 
+# number and check with a condition whether it is greater than 10
+# the output should be almost identical to histogram
+# only with daily number of visits that are greater than 10 
+
+function suspiciousVisitors(){
+	ips=()
+
+	while read -r ioc; do
+		while read -r line; do
+			if [[ "$line" == *"$ioc"* ]]; then
+				ip=$(echo "$line" | awk '{print $1}')
+
+				found=false
+				for existing_ip in "${ips[@]}"; do
+					if [[ "$existing_ip" == "$ip" ]]; then
+						found=true
+						break
+					fi
+				done
+
+				if ! $found; then
+					ips+=("$ip")
+				fi
+			fi
+		done < "$logFile"
+	done < "ioc.txt"
+
+	echo "Unique suspicious IP count: ${ips[@]}"
+}
+# function: suspiciousVisitors
+# Manually make a list of indicators of attack (ioc.txt)
+# filter the records with this indicators of attack
+# only display the unique count of IP addresses.  
+# Hint: there are examples in slides
+
+# Keep in mind that I have selected long way of doing things to 
+# demonstrate loops, functions, etc. If you can do things simpler,
+# it is welcomed.
+
+while :
+do
+	echo "Please select an option:"
+	echo "[1] Display all Logs"
+	echo "[2] Display only IPS"
+	echo "[3] Display only Pages"
+	echo "[4] Histogram"
+	echo "[5] Frequent Visitors"
+	echo "[6] Suspicious Visitors"
+	echo "[7] Quit"
+
+	read userInput
+	echo ""
+
+	if [[ "$userInput" == "7" ]]; then
+		echo "Goodbye"
+		break
+
+	elif [[ "$userInput" == "1" ]]; then
+		echo "Displaying all logs:"
+		displayAllLogs
+
+	elif [[ "$userInput" == "2" ]]; then
+		echo "Displaying only IPS:"
+		displayOnlyIPs
+
+	elif [[ "$userInput" == "3" ]]; then
+		echo "Displaying only pages:"
+		displayOnlyPages
+
+	elif [[ "$userInput" == "4" ]]; then
+		echo "Histogram:"
+		histogram
+
+	elif [[ "$userInput" == "5" ]]; then
+		echo "Frequent visitors:"
+		frequentVisitors
+
+	elif [[ "$userInput" == "6" ]]; then
+		echo "Suspicious visitors:"
+		suspiciousVisitors
+
+	else 
+		echo "You have not entered a valid input. Please try again:"
+
+	fi
+done
