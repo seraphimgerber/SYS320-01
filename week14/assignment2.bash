951a377046d328a6853f68e68efd92780ed26fc7
seraphim gerbs <seraphim.gerber@mymail.champlain.edu>
2025-04-22 18:01:44 -0400

    addding

--- week14/assignment2.bash
+++ week14/assignment2.bash
@@ -3,3 +3,32 @@
 link="http://10.0.17.6/Assignment.html"
 
 fullPage=$(curl -sL "$link")
+
+output=$(echo "$fullPage" | xmlstarlet format --html --recover 2>/dev/null | xmlstarlet select --template --copy-of \
+"//html//body//table[@id='temp']//tr | //html//body//table[@id='press']//tr")
+
+data=$(echo "$output" | sed 's/<\/tr>/\n/g' | sed -e 's/&amp;//g' -e 's/<tr>//g' -e 's/<td[^>]*>//g' -e 's/<\/td>/;/g' \
+-e 's/<[/\]\{0,1\}a[^>]*.\\g' -e 's/<[/\]\{0,1\}nobr>//g' -e 's/^[ \t]*//;s/[ \t]*$//' -e '/^$/d')
+
+tempLines=()
+pressLines=()
+counter=0
+
+while read -r line; do
+	if [[ "$line" == *"Temprature;" || "$line" == *"Pressure;"* ]]; then
+		continue
+	fi
+	if [[ $counter -lt 5 ]]; then
+		tempLines+=("$line")
+	else
+		pressLines+=("${line}")
+	fi
+	((counter++))
+done <<< "$data"
+
+for i in "${!tempLines[@]}"; do
+	tempVal=$(echo "$[tempLines[$i]}" | cut -d';' -f1)
+	datetime=$(echo "${tempLines[$i]}" | cut -d';' -f2)
+	pressVal=$(echo "${pressLines[$i]}" | cut -d';' -f1)
+	echo "$pressVal $tempVal $datetime"
+done
