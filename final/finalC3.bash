fda8b6fe550f81a2821cc7767df0a4d9d49bc640
seraphim gerbs <seraphim.gerber@mymail.champlain.edu>
2025-05-01 12:24:31 -0400

    no, this is.

--- final/finalC3.bash
+++ final/finalC3.bash
@@ -1,38 +1,17 @@
 #!/bin/bash
 
+{
+	echo "<html><body><body style='background-color:#fff2fa;'><h2>Access logs with IOC indicators:</h2><table border='1'>"
+	echo "<tr><th>IP address</td><th>Date and Time</th><th>Page Accessed</th></tr>"
 
-input="report.txt"
-output="report.html"
-dest="/var/www/html/$output"
- 
-cat <<END > "$output"
-<html>
-<head>
-	<title> Access logs w IOC</title>
-	<style>
-		body { background-color: #fff2fa; }
-		table, th, td { border: 1px solid black; border-collapse: collapse; }
-		th, td { padding: 5px; font-size: 17px;
-	</style>
-</head>
-<body>
-<h2> Access logs with IOC indicators:</h2>
-<table>
-END
+	while read -r line; do
+		ip=$(echo "$line" | awk '{print $1}')
+		datetime=$(echo "$line" | awk '{print $2}')
+		page=$(echo "$line" | awk '{print $3}')
+		echo "<tr><td>$ip</td><td>$datetime</td><td>$page</td></tr>"
+	done < report.txt
+	echo "</table></body></html>"
+} > report.html
 
-while read -r line; do
-	ip=$(echo "$line" | awk '{print $1}')
-	datetime=$(echo "$line" | awk '{print $2}')
-	ind=$(echo "$line" | awk '{print $3}')
-	echo "<tr><td>$ip</td><td>$datetime</td><td>$ind</td></tr>" >> "$output"
-done < "$input"
-
-cat <<END >> "$output"
-</table>
-</body>
-<html>
-END
-
-sudo mv "$output" "$dest"
-
-echo "HTML file made and is now in $dest"
+sudo mv report.html /var/www/html
+echo "Report has moved to /var/www/html/report.html!"
